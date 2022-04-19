967d5e90344cc4fd4b421f71fe1d2f7d38b7a2b6
ziko442 <ziko442@gmail.com>
2022-04-19 18:00:05 +0000

    adding userList compo page

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './UserItem.css';
+
+const UserItem = props => {
+//   return (
+//     <div key={props.user.id}>
+//       <img src={props.user.image} alt={props.user.name} />
+//       <div>
+//         <h3>{props.user.name}</h3>
+//         <p>{props.user.company.catchPhrase}</p>
+//         <p>{props.user.email}</p>
+//       </div>
+//     </div>
+//   );
+};
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,8 +1,16 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    return <h2>Users </h2>;
+    const USERS = [
+        { id: 'u1', 
+        name: 'Max', 
+        image: 'https://randomuser.me/api/portraits/men/75.jpg',
+        places: 3 
+    }
+];
+
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
-
