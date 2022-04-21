34412a5c3b3081182788772b7e9603c63fe2d0f0
ziko442 <ziko442@gmail.com>
2022-04-21 01:28:24 +0000

    styling btn and add modals

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -8,7 +8,8 @@ const DUMMY_PLACES = [
     id: 'p1',
     title: 'Empire State Building',
     description: 'One of the most famous sky scrapers in the world!',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
     address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
@@ -20,7 +21,8 @@ const DUMMY_PLACES = [
     id: 'p2',
     title: 'Empire State Building',
     description: 'One of the most famous sky scrapers in the world!',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
     address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
@@ -33,7 +35,6 @@ const DUMMY_PLACES = [
 const UserPlaces = () => {
   const userId = useParams().userId;
   const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
-
   return <PlaceList items={loadedPlaces} />;
 };
 
