f1f25835f91b638356339c4f12d6f94e40e990fa
ziko442 <ziko442@gmail.com>
2022-06-01 20:47:06 +0100

    api delete place

--- node-backend/controllers/places-controllers.js
+++ node-backend/controllers/places-controllers.js
@@ -185,7 +185,7 @@ const deletePlace = async (req, res, next) => {
 
   let place;
   try {
-    place = await Place.findById(placeId);
+    place = await Place.findById(placeId).populate("creator");
   } catch (err) {
     const error = new HttpError(
       "Something went wrong, could not delete place.",
@@ -194,8 +194,21 @@ const deletePlace = async (req, res, next) => {
     return next(error);
   }
 
+  if (!place) {
+    const error = new HttpError(
+      "Could not find place for this id.",
+      404
+    );
+    return next(error);
+  }
+
   try {
-    await place.remove();
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await place.remove({ session: sess });
+    place.creator.places.pull(place);
+    await place.creator.save({ session: sess });
+    await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError(
       "Something went wrong, could not delete place.",
