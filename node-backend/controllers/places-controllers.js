68de9330898c7d296d4288bde33f4c136143e0d0
ziko442 <ziko442@gmail.com>
2022-06-01 20:54:40 +0100

    clean code

--- node-backend/controllers/places-controllers.js
+++ node-backend/controllers/places-controllers.js
@@ -7,19 +7,6 @@ const getCoordsForAddress = require("../util/location");
 const Place = require("../models/place");
 const User = require('../models/user');
 
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
 
 const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
@@ -46,8 +33,7 @@ const getPlaceById = async (req, res, next) => {
   res.json({ place: place.toObject({ getters: true }) }); // => { place } => { place: place }
 };
 
-// function getPlaceById() { ... }
-// const getPlaceById = function() { ... }
+
 
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
