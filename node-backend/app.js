f3157330f5acf3b17af6db0832bb5645b6cb9c60
ziko442 <ziko442@gmail.com>
2022-06-02 19:14:11 +0100

    fix typos

--- node-backend/app.js
+++ node-backend/app.js
@@ -38,7 +38,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect('mongodb+srv://ziko442:zikooo1995.@cluster0.mp06q.mongodb.net/places?retryWrites=true&w=majority')
+  .connect(`mongodb+srv://ziko442:zikooo1995.@cluster0.mp06q.mongodb.net/places?retryWrites=true&w=majority`)
   .then(() => {
     app.listen(5000);
   })
