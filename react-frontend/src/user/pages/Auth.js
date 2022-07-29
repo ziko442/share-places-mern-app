11916535268e5b2a8436fe08bbf768bf7508761b
ziko442 <ziko442@gmail.com>
2022-07-29 19:59:51 +0100

    btns visibility

--- react-frontend/src/user/pages/Auth.js
+++ react-frontend/src/user/pages/Auth.js
@@ -130,7 +130,7 @@ const Auth = () => {
             type="password"
             label="Password"
             validators={[VALIDATOR_MINLENGTH(6)]}
-            errorText="Please enter a valid password, at least 5 characters."
+            errorText="Please enter a valid password, at least 6 characters."
             onInput={inputHandler}
           />
           <Button type="submit" disabled={!formState.isValid}>
