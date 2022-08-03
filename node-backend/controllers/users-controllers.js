f6b01d384efcc1aded62d32e8a995d39cea7e8bf
ziko442 <ziko442@gmail.com>
2022-08-03 17:19:59 +0100

    connecting users to images

--- node-backend/controllers/users-controllers.js
+++ node-backend/controllers/users-controllers.js
@@ -48,7 +48,7 @@ const signup = async (req, res, next) => {
   const createdUser = new User({
     name,
     email,
-    image: "https://picsum.photos/200/300",
+    image: req.file.path,
     password,
     places: [],
   });
