1791090279d0381e235c61799c78c568aa697d04
ziko442 <ziko442@gmail.com>
2022-08-14 19:48:22 +0100

    using environment variable in nodeJS

--- node-backend/app.js
+++ node-backend/app.js
@@ -50,7 +50,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(`mongodb+srv://ziko442:zikooo1995.@cluster0.mp06q.mongodb.net/places?retryWrites=true&w=majority`)
+  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}.@cluster0.mp06q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then(() => {
     app.listen(5000);
   })
